8d425ebde0f5a1fec6bedeb47f8010f284fb808f
Tran Pham <pham.tr@husky.neu.edu>
2020-05-12 00:40:34 -0400

    finished Chapter 2

--- README.md
+++
@@ -1,5 +0,0 @@
-# TranPham-Book
-
-Comprises of screenshots from the book exercises
-
-Chapter 2 Screenshot:
--- server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-app.use(express.static('index.html'));
-
-app.listen(3000, function () {
-console.log('App started on port 3000');
-})
