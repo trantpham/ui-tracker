e4009dde4c756cd651184c485db0c92be85a44f7
Tran Pham <pham.tr@husky.neu.edu>
2020-05-12 23:56:09 -0400

    added code chapter 3

---
+++ Chapter 3/public/App.jsx
@@ -0,0 +1,17 @@
+class HelloWorld extends React.Component {
+  render() {
+    const continents = ['Africa', 'America', 'Asia','Australia', 'Europe'];
+    const helloContinents = Array.from(continents, c => `Hello ${c}!`);
+    const message = helloContinents.join(' ');
+    return (
+      <div title="Outer div">
+      <h1>{message}</h1>
+      </div>
+    );
+  }
+}
+\
+
+const element = <HelloWorld />;
+
+ReactDOM.render(element, document.getElementById('content'));
