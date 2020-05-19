c6bf3800debac7297fa37a63bb18848f38da1015
Tran Pham <pham.tr@husky.neu.edu>
2020-05-18 22:27:39 -0400

    initialize async state and wait call

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,16 @@ class IssueRow extends React.Component {
 class IssueTable extends React.Component {
   constructor() {
     super();
-    this.state = { issues: initialIssues};
+    this.state = { issues: [] };
+  }
+
+  componentDidMount() {
+    this.loadData();
+  }
+
+  loadData() {
+    setTimeout(() => {
+      this.setState({ issues: initialIssues });}, 500);
   }
   render() {
     const issueRows = this.state.issues.map(issue => <IssueRow key={issue.id} issue={issue} />
