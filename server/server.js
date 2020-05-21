3fe1d845a8eaafdbe8808f5521d7e4bc674da1e1
Tran Pham <pham.tr@husky.neu.edu>
2020-05-21 18:47:58 -0400

    integrated list API

--- server/server.js
+++ server/server.js
@@ -4,10 +4,23 @@ const { ApolloServer } = require('apollo-server-express');
 
 let aboutMessage = "Issue Tracker API v1.0";
 
+const issuesDB = [
+  {
+    id: 1, status: 'New', owner: 'Ravan', effort: 5,
+    created: new Date('2019-01-15'), due: undefined,
+    title: 'Error in console when clicking Add',
+  },
+  {
+    id: 2, status: 'Assigned', owner: 'Eddie', effort: 14,
+    created: new Date('2019-01-16'), due: new Date('2019-02-01'),
+    title: 'Missing bottom border on panel',
+  },
+];
 
 const resolvers = {
   Query: {
     about: () => aboutMessage,
+    issueList,
   },
   Mutation: {
     setAboutMessage,
@@ -16,6 +29,10 @@ const resolvers = {
 
 function setAboutMessage(_, { message }) {
   return aboutMessage = message;
+}
+
+function issueList() {
+  return issuesDB;
 }
 
 const server = new ApolloServer({
