d0093b18fc72b56976932dcdaa86fd38495ff07b
Tran Pham <pham.tr@husky.neu.edu>
2020-05-25 00:11:14 -0400

    connect database with website

--- server/server.js
+++ server/server.js
@@ -3,35 +3,8 @@ const express = require('express');
 const { ApolloServer, UserInputError } = require('apollo-server-express');
 const { GraphQLScalarType } = require('graphql');
 const { Kind } = require('graphql/language');
-const { MongoClient } = require{'mongodb'};
-
-const url = 'mongodb://localhost/issuetracker';
-// Atlas URL  - replace UUU with user, PPP with password, XXX with hostname
-// const url = 'mongodb+srv://UUU:PPP@cluster0-XXX.mongodb.net/issuetracker?retryWrites=true';
-// mLab URL - replace UUU with user, PPP with password, XXX with hostname
-// const url = 'mongodb://UUU:PPP@XXX.mlab.com:33533/issuetracker';
-
-let db;
 
 let aboutMessage = "Issue Tracker API v1.0";
-
-const GraphQLDate = new GraphQLScalarType({
-  name: 'GraphQLDate',
-  description: 'A Date() type in GraphQL as a scalar',
-  serialize(value) {
-    return value.toISOString();
-  },
-  parseValue(value) {
-    const dateValue = new Date(value);
-    return isNaN(dateValue) ? undefined : dateValue;
-   },
-  parseLiteral(ast) {
-  if (ast.kind == Kind.STRING) {
-    const value = new Date(ast.value);
-    return isNaN(value) ? undefined : value;
-  }
-  },
-});
 
 const issuesDB = [
   {
@@ -46,6 +19,24 @@ const issuesDB = [
   },
 ];
 
+const GraphQLDate = new GraphQLScalarType({
+  name: 'GraphQLDate',
+  description: 'A Date() type in GraphQL as a scalar',
+  serialize(value) {
+    return value.toISOString();
+  },
+  parseValue(value) {
+    const dateValue = new Date(value);
+    return isNaN(dateValue) ? undefined : dateValue;
+  },
+  parseLiteral(ast) {
+    if (ast.kind == Kind.STRING) {
+      const value = new Date(ast.value);
+      return isNaN(value) ? undefined : value;
+    }
+  },
+});
+
 const resolvers = {
   Query: {
     about: () => aboutMessage,
@@ -58,44 +49,33 @@ const resolvers = {
   GraphQLDate,
 };
 
+function setAboutMessage(_, { message }) {
+  return aboutMessage = message;
+}
+
+function issueList() {
+  return issuesDB;
+}
+
 function issueValidate(issue) {
   const errors = [];
   if (issue.title.length < 3) {
-    errors.push('Field "title" must be at least 3 characters long.')
+    errors.push('Field "title" must be at least 3 characters long.');
   }
-  if (issue.status == 'Assigned' && !issue.owner) {
-    errors.push('Field "owner" is required when status is "Assigned"')
+  if (issue.status === 'Assigned' && !issue.owner) {
+    errors.push('Field "owner" is required when status is "Assigned"');
   }
   if (errors.length > 0) {
     throw new UserInputError('Invalid input(s)', { errors });
   }
-  }
-
+}
 
 function issueAdd(_, { issue }) {
   issueValidate(issue);
   issue.created = new Date();
   issue.id = issuesDB.length + 1;
-  if (issue.status == undefined) issue.status = 'New';
   issuesDB.push(issue);
   return issue;
-}
-
-function setAboutMessage(_, { message }) {
-  return aboutMessage = message;
-}
-
-async function issueList() {
-  const issues = await db.collection('issues').find({}).toArray();
-  return issues;
-}
-
-
-async function connectToDb() {
-  const client = new MongoClient(url, { useNewUrlParser: true });
-  await client.connect();
-  console.log('Connected to MongoDB at', url);
-  db = client.db();
 }
 
 const server = new ApolloServer({
@@ -104,7 +84,7 @@ const server = new ApolloServer({
   formatError: error => {
     console.log(error);
     return error;
-  }
+  },
 });
 
 const app = express();
@@ -112,12 +92,7 @@ const app = express();
 app.use(express.static('public'));
 
 server.applyMiddleware({ app, path: '/graphql' });
-(async function ()
-try {
-  app.listen(3000, function () {
+
+app.listen(3000, function () {
   console.log('App started on port 3000');
-  });
-} catch (err) {
-  console.log('ERROR:', err);
-}
-}) ();
+});
