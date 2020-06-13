525440ec103450489dbd74182bd3ab7a6cf295ba
Tran Pham <pham.tr@husky.neu.edu>
2020-06-13 18:35:34 -0400

    added min/max param for users

--- ui/src/IssueFilter.jsx
+++ ui/src/IssueFilter.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import URLSearchParams from 'url-search-params';
-
 import { withRouter } from 'react-router-dom';
+
 class IssueFilter extends React.Component {
   constructor({ location: { search } }) {
     super();
     const params = new URLSearchParams(search);
     this.state = {
-      stastus: params.get('status') || ``,
+      status: params.get('status') || '',
+      effortMin: params.get('effortMin') || '',
+      effortMax: params.get('effortMax') || '',
       changed: false,
-    },
+    };
+
     this.onChangeStatus = this.onChangeStatus.bind(this);
+    this.onChangeEffortMin = this.onChangeEffortMin.bind(this);
+    this.onChangeEffortMax = this.onChangeEffortMax.bind(this);
     this.applyFilter = this.applyFilter.bind(this);
     this.showOriginalFilter = this.showOriginalFilter.bind(this);
   }
@@ -23,30 +28,50 @@ class IssueFilter extends React.Component {
     }
   }
 
-
   onChangeStatus(e) {
     this.setState({ status: e.target.value, changed: true });
   }
 
-  showOriginalFilter() {
+  onChangeEffortMin(e) {
+    const effortString = e.target.value;
+    if (effortString.match(/^\d*$/)) {
+      this.setState({ effortMin: e.target.value, changed: true });
+    }
+  }
 
+  onChangeEffortMax(e) {
+    const effortString = e.target.value;
+    if (effortString.match(/^\d*$/)) {
+      this.setState({ effortMax: e.target.value, changed: true });
+    }
+  }
+
+  showOriginalFilter() {
     const { location: { search } } = this.props;
-  const params = new URLSearchParams(search);
-  this.setState({
-    status: params.get('status') || ``,
-    changed: false,
-  });
-}
-applyFilter() {
-  const { status } = this.state;
-  const { history } = this.props;
-  history.push({
-    pathname: '/issues',
-    search: status ? `?status=${status}` : ``,
-  });
-}
+    const params = new URLSearchParams(search);
+    this.setState({
+      status: params.get('status') || '',
+      effortMin: params.get('effortMin') || '',
+      effortMax: params.get('effortMax') || '',
+      changed: false,
+    });
+  }
+
+  applyFilter() {
+    const { status, effortMin, effortMax } = this.state;
+    const { history } = this.props;
+    const params = new URLSearchParams();
+    if (status) params.set('status', status);
+    if (effortMin) params.set('effortMin', effortMin);
+    if (effortMax) params.set('effortMax', effortMax);
+
+    const search = params.toString() ? `?${params.toString()}` : '';
+    history.push({ pathname: '/issues', search });
+  }
+
   render() {
-      const { status, changed } = this.state;
+    const { status, changed } = this.state;
+    const { effortMin, effortMax } = this.state;
     return (
       <div>
         Status:
@@ -58,6 +83,20 @@ applyFilter() {
           <option value="Fixed">Fixed</option>
           <option value="Closed">Closed</option>
         </select>
+        {' '}
+        Effort between:
+        {' '}
+        <input
+          size={5}
+          value={effortMin}
+          onChange={this.onChangeEffortMin}
+        />
+        {' - '}
+        <input
+          size={5}
+          value={effortMax}
+          onChange={this.onChangeEffortMax}
+        />
         {' '}
         <button type="button" onClick={this.applyFilter}>Apply</button>
         {' '}
@@ -72,4 +111,5 @@ applyFilter() {
     );
   }
 }
+
 export default withRouter(IssueFilter);
