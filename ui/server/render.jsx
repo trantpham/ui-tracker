312e8d3f25d58b0bfe811b469c3532b741970d86
Tran Pham <pham.tr@husky.neu.edu>
2020-06-26 00:55:56 -0400

    render the Page component wrapped around by a StaticRouter

---
+++ ui/server/render.jsx
@@ -0,0 +1,15 @@
+import { StaticRouter } from 'react-router-dom';
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import About from '../src/Page.jsx';
+import template from './template.js';
+function render(req, res) {
+  const element = (
+	<StaticRouter location={req.url} context={{}}>
+		<Page />
+	</StaticRouter>
+	);
+	const body = ReactDOMServer.renderToString(element);
+  res.send(template(body));
+}
+export default render;
--- ui/server/uiserver.js
+++ ui/server/uiserver.js
@@ -1,10 +1,15 @@
-require('dotenv').config();
-const path = require('path');
-const express = require('express');
-const proxy = require('http-proxy-middleware');
-const render = require('./render.js');
+import dotenv from 'dotenv';
+import path from 'path';
+import express from 'express';
+import proxy from 'http-proxy-middleware';
+import SourceMapSupport from 'source-map-support';
+import render from './render.jsx';
 
 const app = express();
+
+SourceMapSupport.install();
+dotenv.config();
+
 const enableHMR = (process.env.ENABLE_HMR || 'true') === 'true';
 if (enableHMR && (process.env.NODE_ENV !== 'production')) {
   console.log('Adding dev middleware, enabling HMR');
@@ -13,7 +18,7 @@ if (enableHMR && (process.env.NODE_ENV !== 'production')) {
   const webpack = require('webpack');
   const devMiddleware = require('webpack-dev-middleware');
   const hotMiddleware = require('webpack-hot-middleware');
-  const config = require('../webpack.config.js');
+  const config = require('../webpack.config.js')[0];
   config.entry.app.push('webpack-hot-middleware/client');
   config.plugins = config.plugins || [];
   config.plugins.push(new webpack.HotModuleReplacementPlugin());
@@ -33,7 +38,9 @@ const UI_API_ENDPOINT = process.env.UI_API_ENDPOINT
   || 'http://localhost:3000/graphql';
 const env = { UI_API_ENDPOINT };
 
-app.get('/about', render);
+app.get('/about', (req, res, next) => {
+  render(req, res, next);
+});
 app.get('/env.js', (req, res) => {
   res.send(`window.ENV = ${JSON.stringify(env)}`);
 });
@@ -46,3 +53,6 @@ const port = process.env.UI_SERVER_PORT || 8000;
 app.listen(port, () => {
   console.log(`UI started on port ${port}`);
 });
+if (module.hot) {
+  module.hot.accept('./render.jsx');
+}
